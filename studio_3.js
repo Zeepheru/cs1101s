192ec5ce7eea106bd36e39ce2db823340bcc13b6
Zee <No public email provided>
2023-08-28 10:52:31 +0800

    Changes made from Source Academy

---
+++ studio_3.js
@@ -0,0 +1,22 @@
+// S3
+
+import {
+    stack, blank, square, heart, show, beside
+} from "rune";
+
+function f1(rune_1, n, rune_2) {
+    return n === 0
+           ? rune_2
+           : f1(rune_1, n - 1, beside(rune_1, stack(blank, rune_2)));
+}
+
+function f2(rune, n) {
+    return n === 0
+           ? rune
+           : stack(beside(blank, f2(rune, n - 1)),
+                    square);
+}
+
+
+show(f1(square, 3, heart));
+show(f2(heart, 3));
