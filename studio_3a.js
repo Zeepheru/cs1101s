9d8e41369e3afbeb39d4263409f734f21bb8eef2
Zee <No public email provided>
2023-08-28 14:32:48 +0800

    Changes made from Source Academy

---
+++ studio_3a.js
@@ -0,0 +1,42 @@
+// S3a
+
+import {
+    circle, square, show, beside, beside_frac,
+    stackn, blank, ribbon, stack, repeat_pattern, stack_frac
+} from "rune";
+
+function moony_1(bottom_right) {
+    return beside(
+                stack(circle, square),
+                stack(blank, bottom_right));
+}
+
+function moony_2(n) {
+    return n === 1
+            ? circle
+            : moony_1(moony_2(n - 1));
+}
+
+function moony(n) {
+    // anyhow whack also can reach 
+    return n === 0
+            ? circle
+            : beside_frac(
+                1/n,
+                stack_frac(1/n, circle, square),
+                stack_frac(1/n, blank, moony(n - 1)));
+}
+
+
+// show(moony_1(ribbon));
+show(moony(5));
+
+// Qn 4
+/*
+Recursive (since result of function is not 
+the result of the recursive call)
+
+\Theta(n)?
+Since it seems linear
+
+*/
