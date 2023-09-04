136db637b64a0e10755554a2a7ef80e62a0cf43b
Zee <No public email provided>
2023-09-04 14:20:18 +0800

    Changes made from Source Academy

--- studio_4.js
+++ studio_4.js
@@ -2,11 +2,17 @@
 
 function pascal(row, position) {
     // recursive
-    return (position === 0 || row + 1 === position)
-            ? 0
-            : row === 1
+    
+    // note that this is a streamlined version with less conditions, 
+    // but still the right implementation
+    return position === 0 || row === position
             ? 1
             : pascal(row - 1, position) + pascal(row - 1, position - 1);
 }
 
 display(pascal(4, 3));
+
+
+// 3A
+
+
