d116fd6584438eb9c8a2a3f335fd73c78ac8ea00
Source Academy User <Zee>
2023-09-07 01:05:33 +0800

    Changes made from Source Academy

--- refl_4.js
+++ refl_4.js
@@ -1,2 +1,30 @@
 // Reflection 4
 
+const q1a = pair(1, 2);
+const q1b = pair(1, pair(3, pair(5, null)));
+const q1c = pair(pair(pair(3, 2), pair(1, 0)), null);
+const q1d = pair(0, list(1, 2));
+const q1e = list(pair(1, 2), list(4, 5), 3);
+
+// q2 ans
+const q2a = list(1, 2, 3);
+const q2b = pair(1, pair(2, 3));
+const q2c = list(list(1, 2), list(3, 4), list(5, 6));
+
+// q3 qn lists
+const list_q3a = list(7, 6, 5, 4, 3, 2, 1);
+const list_q3b = list(list(7), list(6, 5, 4), list(3, 2), 1);
+const list_q3c = list(7, list(list(list(6, 5, list(list(4)), 3), 2)), 1);
+
+const q3a = head(tail(tail(tail(list_q3a))));
+const q3b = 
+
+
+// answer checking!
+const to_check = q3a;
+
+// draw_data(to_check);
+// display(to_check);
+// display_list(to_check);
+
+to_check;
