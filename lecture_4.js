b907895bf5a6d367e22ce410d867a0d4b5289801
Source Academy User <Zee>
2023-09-06 23:20:32 +0800

    Changes made from Source Academy

--- lecture_4.js
+++ lecture_4.js
@@ -1,3 +1,7 @@
 // Lecture 4: Intro to Data Abstraction
 // pairs and empty lists
 
+// point (x, y) represented as a function
+function make_point(x, y) {
+    return component => component === 0 ? x : y;
+}
