76478a53f4d02a439825edfb923bf463ef29da50
Source Academy User <Zee>
2023-09-11 15:03:00 +0800

    Changes made from Source Academy

--- studio_5a.js
+++ studio_5a.js
@@ -1,6 +1,6 @@
 // Studio 5a
 
-function every_second(lst) {
+function every_second_wrong(lst) {
     return is_null(lst)
             ? null
             : length(lst) % 2 === 1
@@ -8,5 +8,26 @@ function every_second(lst) {
             : pair(head(lst), every_second(tail(lst)));
 }
 
+function every_second(lst) {
+    return is_null(lst) || is_null(tail(lst))
+            ? null 
+            : pair(head(tail(lst)), every_second(tail(tail(lst))));
+}
 
-every_second(list("a", "x", "b", "y", "c", "z", "d"));
+// display(every_second(list("a", "x", "b", "y", "c", "z", "d")));
+// display(every_second(list(1, 2, 3, 4)));
+// display(every_second(list(1, 2, 3, 4, 5)));
+
+function sums(lst) {
+    function sum_list(lst) {
+        return is_null(lst)
+                ? 0
+                : head(lst) + sum_list(tail(lst));
+    }
+    
+    return list(
+            sum_list(every_second(pair(0, (lst)))),
+            sum_list(every_second(lst)));
+}
+
+sums(list(1, 2, 3, 4, 5));
