d9698130911a7cbe65220eb54d0043e2744d2a84
Source Academy User <Zee>
2023-09-13 00:06:20 +0800

    Changes made from Source Academy

--- fucking_around_with_sound_2.js
+++ fucking_around_with_sound_2.js
@@ -1,5 +1,3 @@
-// NEEDS S2 so the lists work!
-
 import { 
     make_sound, play, 
     consecutively, simultaneously,
@@ -16,6 +14,17 @@ import {
 
 
 const encoded_hex = "6fc5c70192bbfd5aa85abbb910870814a002a0b676a02145415188deb41d016f77590984b2ed23be534e9b37733bc";
-const type_1_audio = parse_int(encoded_hex, 16);
+const encoded_t1 = parse_int(encoded_hex, 16);
 
-display(type_1_audio);
+const t1_spacer = 1e7;
+
+function create_sound_list(encoded) {
+    return encoded < t1_spacer 
+            ? pair(encoded, null)
+            : pair(encoded % t1_spacer, create_sound_list(math_floor(encoded / t1_spacer)));
+}
+
+const note_encoded_list = tail(reverse(create_sound_list(encoded_t1)));
+// tail removes front
+display(length(note_encoded_list));
+draw_data(note_encoded_list);
