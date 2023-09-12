fccc28e0ef824768a638265d732a12ea2cbbb7d3
Source Academy User <Zee>
2023-09-13 01:34:27 +0800

    Changes made from Source Academy

--- fucking_around_with_sound_2.js
+++ fucking_around_with_sound_2.js
@@ -20,31 +20,6 @@ import {
 // const encoded_t1 = 4200042162636400001641626364000016417401081690108000011616801160000116167011600001161650108162010816501081650108;
 // const t1_spacer = 1e7;
 
-const test_list_encoded_short = list(
-    914115256, 
-900001256, 
-914115256, 
-900001256, 
-915301008, 
-914801008, 
-900001016, 
-914701016, 
-900001016, 
-914601016, 
-900001016, 
-914401008, 
-914101016, 
-914401016, 
-914601016);
-
-const test_list_encoded = list(914115256, 900001256, 914115256, 900001256, 915301008, 914801008, 900001016, 914701016, 900001016, 914601016, 900001016, 914401008, 914101016, 914401016, 914601016, 913915256, 900001256, 913915256, 900001256, 915301008, 914801008, 900001016, 914701016, 900001016, 914601016, 900001016, 914401008, 914101016, 914401016, 914601016, 913815256, 900001256, 913815256, 900001256, 915301008, 914801008, 900001016, 914701016, 900001016, 914601016, 900001016, 914401008, 914101016, 914401016, 914601016, 913715256, 900001256, 913715256, 900001256, 915301008, 914801008, 900001016, 914701016, 900001016, 914601016, 900001016, 914401008, 914101016, 914401016, 914601016);
-
-// IMPORTANT DEFINED
-const bpm = 120;
-const beat_duration = 1 / bpm * 60;
-const bar_duration = 1 / bpm * 60 * 4;
-//
-
 
 
 // function create_sound_list(encoded) {
@@ -71,45 +46,3 @@ function apply_function_to_list(f, lst) {
 }
 
 
-
-// try and generate sounds from t1 encoded numbers
-function generate_sound_t1(encoded_0) {
-    function get_freq(note_number) {
-        return note_number === 0
-                ? 0
-                : midi_note_to_frequency(note_number);
-    }
-    const encoded = encoded_0 - 9e8;
-    
-    const dd = encoded % 1000;
-    const nn = ((encoded - dd) / 1000) % 100;
-    const sound_type = math_floor(encoded / 1e7);
-    const note_number = math_floor((encoded % 1e7) / 1e5) + 21;
-    
-    // display(dd);
-    // display(nn);
-    // display(note_number);
-    // display(nn/dd);
-    
-    const duration = bar_duration * nn / dd;
-    
-    const note_freq = get_freq(note_number);
-    
-    // can add chords here
-    const sound_func = sound_type === 1
-                        ? square_sound
-                        : sound_type === 2
-                        ? triangle_sound
-                        : sound_type === 3
-                        ? sawtooth_sound
-                        : sound_type === 4
-                        ? sine_sound
-                        : (freq, duration) => silence_sound(duration);
-    
-    return sound_func(note_freq, duration);
-}
-
-const test_sound = apply_function_to_list(generate_sound_t1, test_list_encoded);
-
-
-play(consecutively(test_sound));
