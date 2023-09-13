cbd368b3c747b39813c7bf94026adf986c3207c5
Zee <No public email provided>
2023-09-13 10:59:02 +0800

    Changes made from Source Academy

---
+++ lecture_5.js
@@ -0,0 +1,35 @@
+// Lecture 5
+
+// appending two lists
+function append(xs, ys) {
+    // my own idea? Yes, it is correct!
+    // Time complexity: Theta(n), n is len(xs)
+    // Space complxity: Theta(n) as well, since there are 2n's, but that's still n.
+    return is_null(xs) 
+            ? ys
+            : pair(head(xs), append(tail(xs), ys));
+}
+// list reversing
+function reverse(xs) {
+    // Time complexity: Theta(n^2) Since n times, each time decreasing by 1 from n
+    // So the number of ops is n(n+1)/2
+    return is_null(xs)
+            ? null
+            : append(reverse(tail(xs)), list(head(xs)));
+            // note the second list, since append needs two lists
+}
+
+function reverse2(xs) {
+    // no deferred ops -> iterative
+    // Time complexity: Theta(n)
+    // Space complexity: Theta(n) 
+    // - length of ops is constant, length of new list grows with n
+    function rev(original, reversed) {
+        return is_null(original)
+                ? reversed
+                : rev(tail(original), pair(head(original), reversed));
+    }
+    return rev(xs, null);
+}
+
+reverse2(list(1, 2, 3, 4, 5));
