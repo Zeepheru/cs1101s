fa15cbf27381928ccf0ebefa93db12bf8e97d9b1
Source Academy User <Zee>
2023-09-20 10:12:40 +0800

    Changes made from Source Academy

--- lecture_6.js
+++ lecture_6.js
@@ -1,2 +1,22 @@
 // Lecture 6
 
+const N = 100;
+
+function guess_secret_num(start, end) {
+    // BINARY SEARCH!
+    // O(log n) => since max of k calls for N = 2^k
+    
+    if (start === end) {
+        return start;
+    } else {
+        const guess = math_floor((start + end) / 2);
+        const check = check_guess(guess);
+        return check === "correct"
+        ? guess
+        : check === "too low"
+        ? guess_secret_num(guess + 1, end) // when "too low"
+        : guess_secret_num(start, guess - 1); // when "too high"
+    }
+}
+// guess_secret_num(1, N);
+
