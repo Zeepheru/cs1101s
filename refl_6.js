c4f6192a417f6390aa9a10409a82a3fdc563702e
Source Academy User <Zee>
2023-09-21 08:28:44 +0800

    Changes made from Source Academy

--- refl_6.js
+++ refl_6.js
@@ -1,5 +1,6 @@
 // Reflection 6
 
+// Q1
 function insert_cmp(x, xs, cmp) {
     return is_null(xs)
             ? list(x)
@@ -18,6 +19,18 @@ function insertion_sort_cmp(xs, cmp) {
 
 const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 
-display_list(insertion_sort_cmp(
-                            xs, 
-                            (x, y) => x < y ? x : y));
+display_list(insertion_sort_cmp(xs, (x, y) => x < y));
+display_list(insertion_sort_cmp(xs, (x, y) => x > y));
+display_list(insertion_sort_cmp(xs, (x, y) => false));
+display_list(insertion_sort_cmp(xs, (x, y) => x % 2 === 0 ? x < y : x > y));
+
+// Q2
+
+/*
+
+
+
+
+
+
+*/
