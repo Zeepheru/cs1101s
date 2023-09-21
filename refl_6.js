af94ab248a7ee39861e9cd0630201c7a0011e2d1
Source Academy User <Zee>
2023-09-21 09:31:23 +0800

    Changes made from Source Academy

--- refl_6.js
+++ refl_6.js
@@ -20,6 +20,7 @@ function insertion_sort_cmp(xs, cmp) {
 
 const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 
+// "when is x in front"
 display_list(insertion_sort_cmp(xs, (x, y) => x < y));
 display_list(insertion_sort_cmp(xs, (x, y) => x > y));
 display_list(insertion_sort_cmp(xs, (x, y) => false));
