79ffb564a79c8d41ca6e9815bf099b51e2228d43
Source Academy User <Zee>
2023-09-25 21:33:46 +0800

    Changes made from Source Academy

--- fucking_around_with_sound_4.js
+++ fucking_around_with_sound_4.js
@@ -185,15 +185,14 @@ function chorus(sound) {
     const lfo_freq = 17;
     
     function lfo_creator(delay_min, delay_max) {
-        // returns the delayed time!
-        return t => t + (delay_max - delay_min) / 2000 * math_sin(twopi * lfo_freq * t);
+        return t => (delay_max - delay_min) / 2000 * math_sin(twopi * lfo_freq * t);
     }
     
     const lfo = lfo_creator(15, 35);
     const sound_dur = get_duration(sound);
     const sound_wave = get_wave(sound);
     
-    return make_sound(t => sound_wave(t) + sound_wave(lfo(t)), sound_dur);
+    return make_sound(t => sound_wave(t) + sound_wave(t + lfo(t)), sound_dur);
 }
 
 // NOTE TESTING
