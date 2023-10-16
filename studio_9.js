0697020abc05bd029bfd534b41b8730312240bf6
Source Academy User <Zee>
2023-10-16 10:56:06 +0800

    Changes made from Source Academy

--- studio_9.js
+++ studio_9.js
@@ -11,21 +11,22 @@ original x when change(x, 1) is called.
 */
 
 // Q2
-// function d_filter(pred, xs) {
-//     if (is_null(xs)) {
-//         return xs;
-//     } else if (pred(head(xs))) {
-//         set_tail(xs, d_filter(pred, tail(xs)));
-//         return xs;
-//     } else {
-//         return d_filter(pred, tail(xs));
-//     }
-// }
+function d_filter(pred, xs) {
+    if (is_null(xs)) {
+        return xs;
+    } else if (pred(head(xs))) {
+        set_tail(xs, d_filter(pred, tail(xs)));
+        return xs;
+    } else {
+        return d_filter(pred, tail(xs));
+    }
+}
 
-// const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
-// const q2 = d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
-// // L; // What is L now?
+const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
+const q2 = d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
+// L; // What is L now?
 
 // q2;
 
 // Q3
+// check drawings lol, breakpoints are not easy to code into Source. 
