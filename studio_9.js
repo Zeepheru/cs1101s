0ccaf93326d2f213d385a89b1f385d39a977ef3c
Source Academy User <Zee>
2023-10-16 10:29:42 +0800

    Changes made from Source Academy

--- studio_9.js
+++ studio_9.js
@@ -13,4 +13,4 @@ const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
 const test_ans = d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
 // L; // What is L now?
 
-draw_data(test_ans);
+test_ans;
