a59be9e4655dfcbff2cfcb7feba0e152b5970b7b
Source Academy User <Zee>
2023-10-16 10:44:40 +0800

    Changes made from Source Academy

--- studio_9.js
+++ studio_9.js
@@ -10,21 +10,22 @@ x is substituted replaced by 0, not the reference to the
 original x when change(x, 1) is called. 
 */
 
-function d_filter(pred, xs) {
-    if (is_null(xs)) {
-        return xs;
-    } else if (pred(head(xs))) {
-        set_tail(xs, d_filter(pred, tail(xs)));
-        return xs;
-    } else {
-        return d_filter(pred, tail(xs));
-    }
-}
+// Q2
+// function d_filter(pred, xs) {
+//     if (is_null(xs)) {
+//         return xs;
+//     } else if (pred(head(xs))) {
+//         set_tail(xs, d_filter(pred, tail(xs)));
+//         return xs;
+//     } else {
+//         return d_filter(pred, tail(xs));
+//     }
+// }
 
-const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
-const test_ans = d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
-// L; // What is L now?
+// const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
+// const q2 = d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
+// // L; // What is L now?
 
-test_ans;
+// q2;
 
 // Q3
