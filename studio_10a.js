77391a2e25674f23aef0b7801f0e2771b55521b5
Source Academy User <Zee>
2023-10-23 15:35:19 +0800

    Changes made from Source Academy

--- studio_10a.js
+++ studio_10a.js
@@ -18,35 +18,55 @@ function rotate(M) {
     const max_n = array_length(M);
     const mid = (max_n - 1) / 2;
     
-    for (let i = 0; i < max_n; i = i + 1) {
-        for (let j = 0; j < max_n; j = j + 1) {
+    const mid_n = math_floor(array_length(M));
+    
+    for (let i = 0; i < mid_n; i = i + 1) {
+        for (let j = 0; j < mid_n; j = j + 1) {
             
             const i0 = i - mid;
             const j0 = j - mid;
             
-            let theta = math_atan(j0 / i0);
-            theta = i0 === 0 ? math_PI : theta;
+            let theta = math_atan(i0 / j0);
+            theta = j0 === 0 ? 0 : theta;
             
             const point_r = math_sqrt(i0 * i0 + j0 * j0);
-            const theta_n = theta - math_PI / 2;
+            const theta_n = theta + math_PI / 2;
             
             display(point_r);
-            display(theta);
+            display(stringify(theta / math_PI * 180) + " --> " + stringify(theta_n / math_PI * 180));
             
             
             let i1 = point_r * math_cos(theta_n);
             let j1 = point_r * math_sin(theta_n);
-            i1 = math_round(i1 + mid);
-            j1 = math_round(j1 + mid);
+            // i1 = math_round(i1 + mid);
+            // j1 = math_round(j1 + mid);
             
-            display(stringify(i) + " . " + stringify(j));
+            // display(stringify(i) + " . " + stringify(j));
+            display(stringify(i0) + " . " + stringify(j0));
             display(stringify(i1) + " . " + stringify(j1));
             display("__________________");
             
-            swap(M, i, j, i1, j1);
+            // swap(M, i, j, i1, j1);
         }
+        break;
     }
     return M;
+}
+
+function rotate_v2(M) {
+    const n = array_length(M);
+    for (let i = 0; i < n; i = i + 1) {
+        for (let j = 0; j < n; j = j + 1) {
+            swap(M, i, j, j, i);
+        }
+    }
+    
+    const half_n = math_floor(n);
+    
+    for (let r = 0; r < n; r = r + 1) {
+        // reverse_array(M[r]);
+    }
+    
 }
 
 rotate(M2);
